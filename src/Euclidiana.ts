3a9a0c68f7e8f4581e6f7cb1e847bb505c83e6dd
Juan Cassiano <juancassiano@hotmail.com>
2022-03-31 11:12:13 -0300

    list all address and get the lat and long

---
+++ src/Euclidiana.ts
@@ -0,0 +1,3 @@
+function Euclidiana(addresses[]);
+
+export { Euclidiana };
--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,39 @@
 import express from "express";
 import axios from "axios";
 
-const API_KEY = "AIzaSyApj0HNYssirk2L1qDU56WInzJrIptGoug";
+const API_KEY = "&key=AIzaSyApj0HNYssirk2L1qDU56WInzJrIptGoug";
+const LANGUAGE = "&Language=pt-BR";
 const BASE_URL = "https://maps.googleapis.com/maps/api/geocode/json?address=";
 const app = express();
+
+const geolocations = [];
+const adresses = [];
+
 app.use(express.json());
 
 app.get("/", async (request, response) => {
-  const { data } = await axios(
-    `${BASE_URL}94+Rua+Fernandes+Gusmao,+Rio+De+Janeiro,
-    +RJ&Language=pt-BR&key=${API_KEY}`
+  const geolocationURL = await axios(
+    `${BASE_URL}94 Rua Fernandes Gusmao, Rio De Janeiro,${LANGUAGE}
+    ${API_KEY}`
   );
-  return response.json(data.results[0].geometry.location);
-  // return response.json({ data.results.geometry.location });
+  return response.json(geolocationURL.data.results[0].geometry.location);
+});
+
+app.post("/route", async (request, response) => {
+  const { number, address, city, state } = request.body;
+
+  const fullAddress = `${number} ${address}, ${city}, ${state}`;
+
+  const geolocationURL = await axios(
+    `${BASE_URL}${fullAddress}${LANGUAGE}${API_KEY}`
+  );
+
+  adresses.push(fullAddress);
+  return response.json(geolocationURL.data.results[0].geometry.location);
+});
+
+app.get("/route", async (request, response) => {
+  return response.json(adresses);
 });
 
 app.listen("3000", () => {
